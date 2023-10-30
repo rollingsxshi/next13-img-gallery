899082c6381d1c491f9b45a4b68871c218c95af5
rollingsxshi <rollingsxshi@gmail.com>
2023-10-30 23:47:43 +0800

    pagination & upgrade to next14

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -9,7 +9,7 @@ const Search = () => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    router.push(`/results/${search}`);
+    if (search) router.push(`/results/${search}`);
     setSearch("");
   };
 
