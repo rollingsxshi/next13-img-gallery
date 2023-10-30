87dda4ee57d957ea2f4af19ab0d85ff56adea024
rollingsxshi <rollingsxshi@gmail.com>
2023-10-30 23:48:36 +0800

    edit navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="bg-black sticky top-0 z-10">
       <nav className="flex flex-col gap-4 sm:flex-row sm:justify-between items-center p-4 font-bold max-w-6xl mx-auto text-white">
         <h1 className="text-2xl sm:text-3xl text-center whitespace-nowrap">
-          <Link href={"/"}>Next13 Image Gallery</Link>
+          <Link href={"/"}>Next14 Image Gallery</Link>
         </h1>
         <Search />
       </nav>
